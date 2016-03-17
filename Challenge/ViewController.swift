15e0512f36448686fc340e643b1defcdd6bee057
Pedro Henrique Prates Peralta <pedro@ckl.io>
2016-03-16 22:08:37 -0300

    Initial Commit

---
+++ Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Challenge
+//
+//  Created by Pedro Henrique Prates Peralta on 3/16/16.
+//  Copyright © 2016 Cheesecake Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
