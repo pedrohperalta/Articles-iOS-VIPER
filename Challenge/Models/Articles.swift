30bccad8a810a6588deb76f10f0e41b28ea26989
Pedro Henrique Prates Peralta <pedro@ckl.io>
2016-03-16 23:19:55 -0300

    Creates article model

---
+++ Challenge/Models/Articles.swift
@@ -0,0 +1,35 @@
+//
+//  Articles.swift
+//  Challenge
+//
+//  Created by Pedro Henrique Prates Peralta on 3/16/16.
+//  Copyright © 2016 Cheesecake Labs. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+
+public class Article : Mappable
+{
+    var title: String?
+    var website: String?
+    var authors: String?
+    var content: String?
+    var imageUrl: String?
+    var date: NSDate?
+
+
+    required public init?(_ map: Map) {
+    }
+
+
+    public func mapping(map: Map) {
+        title       <- map["title"]
+        website     <- map["website"]
+        authors     <- map["authors"]
+        content     <- map["content"]
+        imageUrl    <- map["image"]
+        date        <- (map["date"], DateTransform())
+    }
+}
--- Podfile
+++ Podfile
@@ -3,7 +3,8 @@ platform :ios, '8.0'
 use_frameworks!
 
 target 'Challenge' do
-
+    
     pod 'SnapKit', '~> 0.15.0'
+    pod 'ObjectMapper', '~> 1.1'
 
 end
